67d4cdabbe00170a33e9fb12c12e42318577365b
DavidJurenec <david.jurenec@gmail.com>
2024-03-31 17:12:03 +0200

    Added reading from input file

--- main.cpp
+++ main.cpp
@@ -4,8 +4,24 @@
 #include <cstring>
 
 int main(int argc, char* argv[]) {
+     if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input file>" << std::endl;
+        return 1;
+    }
 
-    std::cout << "Hello World!";
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile) {
+        std::cerr << "Error opening input file." << std::endl;
+        return 1;
+    }
+
+    std::vector<unsigned char> A;
+    unsigned int temp;
+    while (inputFile >> temp) {
+        A.push_back(static_cast<unsigned char>(temp));
+    }
+
+    inputFile.close();
 
     return 0;
 }
