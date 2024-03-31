17f08601a74dba8af3181e990108ed79938e1691
DavidJurenec <david.jurenec@gmail.com>
2024-03-31 18:29:18 +0200

    Modified reading and writing into functions

--- main.cpp
+++ main.cpp
@@ -3,12 +3,49 @@
 #include <vector>
 #include <cstring>
 
+// Funkcija za branje vhodne datoteke
+bool readFile(const char* filename, std::vector<unsigned char>& A) {
+    std::ifstream inputFile(filename); // Vhodna datoteka
+    // Preverjanje vhodne datoteke
+    if (!inputFile) {
+        std::cerr << "Error opening input file." << std::endl;
+        return false;
+    }
+
+    unsigned int temp; // Začasna spremenljivka za branje števil
+
+    // Branje števil iz datoteke
+    while (inputFile >> temp) {
+        A.push_back(static_cast<unsigned char>(temp)); // Pretvori v unsigned char in dodaj v vektor
+    }
+    inputFile.close(); // Zapri vhodno datoteko
+    return true; 
+}
+
+// Funkcija za pisanje števil v datoteko
+bool writeFile(const char* filename, const std::vector<unsigned char>& A) {
+    std::ofstream outputFile(filename); // Izhodna datoteka
+    // Preverjanje izhodne datoteke
+    if (!outputFile) {
+        std::cerr << "Error opening output file." << std::endl;
+        return false;
+    }
+
+    // Zapiši sortirana števila v izhodno datoteko
+    for (size_t i = 0; i < A.size(); i++) {
+        // Izpiši števila ločena s presledki
+        outputFile << static_cast<int>(A[i]) << (i < A.size() - 1 ? ' ' : '\n');
+    }
+    outputFile.close(); // Zapri izhodno datoteko
+    return true;
+}
+
 // Funkcija za sortiranje glede na k-ti bit
 void countingSort(std::vector<unsigned char>& A, int k) {
     int C[2] = {0}; // Polje za štetje kolikokrat se pojavi 0 in 1 na k-tem bitu
     std::vector<unsigned char> B(A.size()); // Začasni vektor za shranjevanje sortiranih elementov
 
-    // Štetj pojavitev bitov
+    // Štetje pojavitve bitov
     for (size_t i = 0; i < A.size(); i++) {
         C[(A[i] >> k) & 1]++; // Izračunaj vrednost k-tega bita in povečaj števec
     }
@@ -34,45 +71,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Vhodna datoteka
-    std::ifstream inputFile(argv[1]); 
+    // Vektor za shranjevanje vhodnih števil
+    std::vector<unsigned char> A; 
 
-    // Preverjanje vhodne datoteke
-    if (!inputFile) {
-        std::cerr << "Error opening input file." << std::endl;
+    // Uporabi funkcijo readFile za branje datoteke
+    if (!readFile(argv[1], A)) { 
         return 1;
     }
     
-    std::vector<unsigned char> A; // Vektor za shranjevanje vhodnih števil
-    unsigned int temp; // Začasna spremenljivka za branje števil
-    while (inputFile >> temp) { // Branje števil iz datoteke
-        A.push_back(static_cast<unsigned char>(temp)); // Pretvori v unsigned char in dodaj v vektor
-    }
-
-    // Zapri vhodno datoteko
-    inputFile.close(); 
-
-    // Izvedi Binarni Radix Sort za vsak bit
+    // Binarni radix sort za vsak bit
     for (int k = 0; k < 8; k++) {
         countingSort(A, k); // Sortiraj vektor A glede na k-ti bit
     }
 
-    // Izhodna datoteka
-    std::ofstream outputFile("out.txt");
-
-    // Preverjanje izhodne datoteke
-    if (!outputFile) {
-        std::cerr << "Error opening output file." << std::endl;
+    // Uporabi funkcijo writeFile za pisanje v datoteko
+    if (!writeFile("out.txt", A)) { 
         return 1;
     }
-
-    // Zapiši sortirana števila v izhodno datoteko
-    for (size_t i = 0; i < A.size(); i++) {
-        outputFile << static_cast<int>(A[i]) << (i < A.size() - 1 ? ' ' : '\n'); // Izpiši števila ločena s presledki
-    }
-
-    // Zapri izhodno datoteko
-    outputFile.close(); 
 
     return 0;
 }
